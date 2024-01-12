ff32d5f519081382bef85fb0932cbbee402749d1
Adi8Hub <63539030+Adi8Hub@users.noreply.github.com>
2024-01-13 02:02:35 +0530

    Create Merge 2 LL.cs

---
+++ Linked List/Merge 2 LL.cs
@@ -0,0 +1,36 @@
+/*
+https://leetcode.com/problems/merge-two-sorted-lists/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode result = new ListNode();
+
+        if(l1.val < l2.val)
+        {
+            result = l1;
+            result.next = MergeTwoLists(l1.next,l2);
+        }
+        else
+        {
+            result = l2;
+            result.next = MergeTwoLists(l1,l2.next);
+        }
+
+        return result;
+    }       
+}
