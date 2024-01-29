43f1fb13c814bc348396e8f4373df947b1d2040c
Adi8Hub <63539030+Adi8Hub@users.noreply.github.com>
2024-01-29 21:47:03 +0530

    Update K Inversion Pairs Array.cs

--- Recursion-Memo-DP/K Inversion Pairs Array.cs
+++ Recursion-Memo-DP/K Inversion Pairs Array.cs
@@ -1,8 +1,17 @@
-Pending DP and DP optimized
+Pending DP optimized: 
+Optimal DP approach:
+//Approach-3 - DP with cumulative sum approach
+//Time : O(n*k)
+//S.C : O(n*k)
+- No need of inv loop
+- use cumulativeSum, where cumSum += prevRow,i.e.,t[i-1][j] 
+if(j>=i) then cumSum -= t[i-1][j-i]
 
 // https://leetcode.com/problems/k-inverse-pairs-array
 
-// 1. Recursion
+//Approach-1 (Recursion+Memo)
+//T.C  : O(k^n)  --> for every n,we are operating on k combinations
+//S.C  : tree depth or recursion stack
 public class Solution {
     int mod = 1000000007;
     public int KInversePairs(int n, int k) {
@@ -27,6 +36,8 @@ public class Solution {
 }
 
 // 2. Memo
+//T.C  : O(n*k*n)
+//S.C  : O(n*k) -> memo + Recursion call stack
 public class Solution {
     int mod = 1000000007;
     int[,] t =new int[1001,1001];
@@ -59,3 +70,39 @@ public class Solution {
         return t[n,k]=result;
     }
 }
+
+
+// 3. DP
+//T.C  : O(n*k*n)
+//S.C  : O(n*k) for memo + Recursion call stack
+public class Solution {
+    public int KInversePairs(int n, int k) 
+    {
+        int mod = 1000000007;
+        // t[i,j] indicates total number of arrays consisting from 1 to i and exactly j inverse pairs
+        int[,] t =new int[n+1,k+1];
+
+        // if 0 inversions, then 1 . i.e.,for j=0;t[i,0]=1 
+        for(int i =0;i<=n;i++)
+        {
+            t[i,0] = 1;
+        }
+
+        // 1st row i.e.0th row, 0 elements= 0 arrays
+        // 1st column i.e.0th col, 0 inversions= 1 arrays present
+        // start from row1,col1
+        for(int i =1;i<=n;i++)
+        {
+            for(int j =1;j<=k;j++)
+            {
+                for(int inv =0;inv<=Math.Min(j,i-1);inv++)
+                {
+                    t[i,j] = (t[i,j]+ t[i-1,j-inv]%mod)%mod;
+                }
+            }
+        }
+        return t[n,k];
+    }        
+}
+
+
