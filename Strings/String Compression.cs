1e676c88f56fa3d597e44cfb596ca630154367d0
Adi8Hub <63539030+Adi8Hub@users.noreply.github.com>
2024-02-05 18:44:13 +0530

    Create String Compression.cs

---
+++ Strings/String Compression.cs
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/string-compression
+
+public class Solution {
+    public int Compress(char[] chars) {
+        int i = 0;
+        int n = chars.Length;
+
+        int currIdx=0;
+        while(i<n)
+        {
+            int count=0;
+            char currChar = chars[i];
+            
+
+            // Count repeating chars
+            while(i<n && currChar==chars[i])
+            {
+                count++;
+                i++;
+            }
+
+            // Assign char to the index and move to next index
+            chars[currIdx++] = currChar;
+            
+            // If count>1, assign count to the index in string format
+            if(count>1)
+            {
+                string cnt = count.ToString();
+
+                foreach(var c in cnt)
+                {
+                    chars[currIdx++] = c;
+                }
+            }
+        }
+        return currIdx;
+
+    }
+}
