aa73050a603bf85176ded527dc5290d467c41be8
Adi8Hub <63539030+Adi8Hub@users.noreply.github.com>
2024-02-09 18:51:16 +0530

    Update Largest Divisible Subset.cs

--- Recursion-Memo-DP/Largest Divisible Subset.cs
+++ Recursion-Memo-DP/Largest Divisible Subset.cs
@@ -32,3 +32,52 @@ public class Solution {
         solve(nums,prev,curr+1,ans,temp);
     }
 }
+
+
+// 2. Bottom -UP DP
+// TC/SC n^2 and n
+public class Solution {
+    public IList<int> LargestDivisibleSubset(int[] nums) 
+    {
+        int n = nums.Length;
+        // LIS - Longest Increasing Subsequence
+        Array.Sort(nums);
+
+        int[] LIS = new int[n];// gives lIS length
+        int[] prev = new int[n];//tracks prev Index < currindex at curr location
+        int maxLength=1;
+        int lastIndex=-0
+        Array.Fill(LIS,1);
+        Array.Fill(prev,-1);
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[i]%nums[j]==0)//in LIS problems, we check for greater no,here as per Q,we check divisibility
+                {
+                    if(LIS[i]<LIS[j]+1)//Update LIS and prev
+                    {
+                        LIS[i] = LIS[j]+1;
+                        prev[i] = j;
+                    }
+
+                    //Track maxLength
+                    if(LIS[i]>maxLength)
+                    {
+                        maxLength = LIS[i];
+                        lastIndex = i;
+                    }
+                }
+            }
+        }
+
+        List<int> ans = new List<int>();
+        while(lastIndex>=0)
+        {
+            ans.Add(nums[lastIndex]);
+            lastIndex = prev[lastIndex];
+        }
+        return ans;
+    }
+}
