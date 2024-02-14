92fa9befcd89f300a0f399b768423738575603f3
Adi8Hub <63539030+Adi8Hub@users.noreply.github.com>
2024-02-15 00:42:32 +0530

    Update KMP Algorithm.cs

--- Strings/KMP Algorithm.cs
+++ Strings/KMP Algorithm.cs
@@ -5,3 +5,10 @@ https://www.geeksforgeeks.org/problems/search-pattern0205/1
 // Brute Force
 // TC: O(m*n)
 // For every m in str1, we check for chars in str2
+
+
+
+
+
+// KMP ALgorithm
+// First fill LPS array ie. Longest Prefix which is also suffix using str2
