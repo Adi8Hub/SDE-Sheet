542551e22ff028ac0c1fc15dbe892665fd027e40
Adi8Hub <63539030+Adi8Hub@users.noreply.github.com>
2024-02-15 18:05:06 +0530

    Update KMP Algorithm.cs

--- Strings/KMP Algorithm.cs
+++ Strings/KMP Algorithm.cs
@@ -5,7 +5,22 @@ https://www.geeksforgeeks.org/problems/search-pattern0205/1
 // Brute Force
 // TC: O(m*n)
 // For every m in str1, we check for chars in str2
+Loop:
+  for (int i = 0; i <= N - M; i++) 
+  {
+      int j;
 
+      /* For current index i, check for pattern
+      match */
+      for (j = 0; j < M; j++)
+          if (txt[i + j] != pat[j])
+              break;
+
+      // if pat[0...M-1] = txt[i, i+1, ...i+M-1]
+      if (j == M)
+          Console.WriteLine("Pattern found at index "
+                            + i);
+    }
 
 
 
