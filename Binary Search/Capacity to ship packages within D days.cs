8aa378023e8a3c1adafcd3b570868aabb61a85d6
Adi8Hub <63539030+Adi8Hub@users.noreply.github.com>
2024-02-22 21:45:04 +0530

    Create Capacity to ship packages within D days.cs

---
+++ Binary Search/Capacity to ship packages within D days.cs
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/description/
+
+// Brute Force
+public class Solution {
+    public int ShipWithinDays(int[] weights, int days) {
+        int n = weights.Length;
+        int left = int.MinValue;
+        int right = 0;
+        // left and right denotes minimum and maximum capacity
+        for(int i = 0;i<n;i++)
+        {
+            left = Math.Max(left,weights[i]);
+            right += weights[i];
+        }
+
+        for(int i = left;i<=right;i++)
+        {
+            int d = f(weights,i);
+            if(d<=days)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    int f(int[] weights, int capacity)
+    {
+        int days=1;
+        int currWt = 0;
+
+        for(int i =0;i<weights.Length;i++)
+        {
+            if(currWt+weights[i]<=capacity)
+            {
+                currWt+=weights[i];
+                
+            }
+            else
+            {
+                currWt=weights[i];
+                days++;
+            }
+        }
+        return days;
+    }
+}
