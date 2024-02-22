1ab25e5cd9d7e56a2483b09c13c4ecb8d80c0126
Adi8Hub <63539030+Adi8Hub@users.noreply.github.com>
2024-02-22 21:50:20 +0530

    Update Capacity to ship packages within D days.cs

--- Binary Search/Capacity to ship packages within D days.cs
+++ Binary Search/Capacity to ship packages within D days.cs
@@ -45,3 +45,47 @@ public class Solution {
         return days;
     }
 }
+
+
+// Binary Search
+public class Solution {
+    public int ShipWithinDays(int[] weights, int days) {
+        int n = weights.Length;
+        int left = int.MinValue;
+        int right = 0;
+        for(int i = 0;i<n;i++)
+        {
+            left = Math.Max(left,weights[i]);
+            right += weights[i];
+        }
+
+        while(left<=right)
+        {
+            int mid = left+(right-left)/2;
+            int d = f(weights,mid);
+            if(d<=days)
+                right = mid-1;
+            else
+                left=mid+1;
+        }
+        return left;
+    }
+
+    int f(int[] weights, int capacity)
+    {
+        int days=1;
+        int currWt = 0;
+
+        for(int i =0;i<weights.Length;i++)
+        {
+            if(currWt+weights[i]<=capacity)
+                currWt+=weights[i];    
+            else
+            {
+                currWt=weights[i];
+                days++;
+            }
+        }
+        return days;
+    }
+}
