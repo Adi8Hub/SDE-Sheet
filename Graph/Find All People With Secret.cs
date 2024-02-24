8fb89f8f597b81cc13b6c64febb74d4828ac813c
Adi8Hub <63539030+Adi8Hub@users.noreply.github.com>
2024-02-25 00:06:34 +0530

    Create Find All People With Secret.cs

---
+++ Graph/Find All People With Secret.cs
@@ -0,0 +1,91 @@
+// https://leetcode.com/problems/find-all-people-with-secret/description
+
+// BFS
+//T.C : ~O(M*(M+N)) where M = number of meetings and N = number of people
+//S.C : O(M+N)
+public class Solution {
+    public List<int> FindAllPeople(int n, int[][] meetings, int firstPerson) 
+    {
+        SortedDictionary<int, List<int[]>> timeMeetings = new SortedDictionary<int, List<int[]>>();
+        bool[] knowsSecret = new bool[n];
+        knowsSecret[0]=true;
+        knowsSecret[firstPerson]=true;
+        
+        // Fill map from the matrix
+        foreach(var meeting in meetings)
+        {
+            int x = meeting[0];
+            int y = meeting[1];
+            int t = meeting[2];
+
+            if(!timeMeetings.ContainsKey(t))
+                timeMeetings[t] = new List<int[]>();
+
+            timeMeetings[t].Add(new int[]{x,y});
+        }
+
+        // Create graph for each time using adj list 
+        // Add starting point for each graph 
+        // BFS traversal for each graph
+        foreach(var t in timeMeetings.Keys)
+        {
+            Dictionary<int,List<int>> meetingAdjList = new Dictionary<int,List<int>>();
+            foreach(var meets in timeMeetings[t])
+            {
+                int x = meets[0];
+                int y = meets[1];
+
+                if(!meetingAdjList.ContainsKey(x))
+                {
+                    meetingAdjList[x] = new List<int>();
+                }
+                meetingAdjList[x].Add(y);
+
+                if(!meetingAdjList.ContainsKey(y))
+                {
+                    meetingAdjList[y] = new List<int>();
+                }
+                meetingAdjList[y].Add(x);
+            }
+
+            // Add starting person in a set to avoid duplicates, using the map
+            HashSet<int> start = new HashSet<int>();
+            foreach(var m in timeMeetings[t])
+            {
+                if(knowsSecret[m[0]])
+                    start.Add(m[0]);
+
+                if(knowsSecret[m[1]])
+                    start.Add(m[1]);
+            }
+
+            // BFS traversal--> next person who knows the secret to be added to Queue
+            Queue<int> q = new Queue<int>(start);
+            while(q.Count>0)
+            {
+                int person = q.Dequeue();
+                if(meetingAdjList.ContainsKey(person))
+                {
+                    foreach(int nextPerson in meetingAdjList[person])
+                    {
+                        if(!knowsSecret[nextPerson])
+                        {
+                            knowsSecret[nextPerson] = true;
+                            q.Enqueue(nextPerson);
+                        }
+                    }
+                }                
+            }
+
+        }
+
+        List<int> ans = new List<int>();
+        for(var i=0;i<n;i++)
+        {
+            if(knowsSecret[i])
+                ans.Add(i);
+        }
+        return ans;        
+    }
+}
+
