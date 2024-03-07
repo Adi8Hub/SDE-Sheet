76f07f41dd0630e1e0a87b2468f0f1b707859b9c
Adi8Hub <63539030+Adi8Hub@users.noreply.github.com>
2024-03-08 01:19:53 +0530

    Update Length of LIS.cs

--- Recursion-Memo-DP/LIS/Length of LIS.cs
+++ Recursion-Memo-DP/LIS/Length of LIS.cs
@@ -66,3 +66,31 @@ public class Solution {
         return Math.Max(take,skip);
     }
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// DP- Bottom UP
+public class Solution {
+    public int LengthOfLIS(int[] nums) {
+        // Complete it using REc+Memo,DP-BU,Patience Sorting-Bucket
+        
+        // O(n^2)
+        int n = nums.Length;
+        // DP- Bottom  UP
+        int[] t = new int[n];
+
+        Array.Fill(t,1);
+        int maxLIS=1;
+        for(int i =0;i<n;i++)
+        {
+            for(int j =0;j<i;j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                    t[i]= Math.Max(t[i],t[j]+1);
+                    maxLIS = Math.Max(maxLIS,t[i]);
+                }
+            }
+        }
+        return maxLIS;   
+    }
+}
